f579691001e084ab05adbb0e3d23ef1b06791c04
Tessa Painter <tessa@tessapainter.com>
2026-09-13 00:10:29 -0500

    thumbs should generate with actions now

--- utils/GenerateThumbnails.js
+++ utils/GenerateThumbnails.js
@@ -4,18 +4,18 @@ import crochetImages from '../assets/crochetImages.json' with { type: "json" };
 import { mkdir } from 'fs/promises'
 
 async function generateThumbnails(basePath = '', images, maxHeight = 250) {
-	await mkdir(`${basePath}/thumbnails`, { recursive: true })
-	images.forEach(image => {
-			console.log(`${basePath}/${image}`)
-			try {
-				sharp(`${basePath}/${image}`)
-					.resize(null, maxHeight)
-					.toFile(`${basePath}/thumbnails/${image}`, )
-					.catch(e => console.error(e));
-			} catch (error) {
-				console.log(`${error} Failed to generate for ${basePath}/${image}`)
-			}
-	});
+	const outputDir = `${basePath}/thumbnails`
+	await mkdir(outputDir, { recursive: true })
+
+	await Promise.allSettled(images.map(async (image) => {
+		const inputPath = `${basePath}/${image}`
+		const outputPath = `${outputDir}/${image}`
+		console.log(`Generating thumbnail for ${inputPath}`)
+		await sharp(inputPath)
+			.resize(null, maxHeight)
+			.toFile(outputPath)
+	}))
 }
-generateThumbnails('static/images/florian', florianImages.map(image => image.path))
-generateThumbnails('static/images/crochet', crochetImages.map(image => image.path))
+
+await generateThumbnails('static/images/florian', florianImages.map(image => image.path))
+await generateThumbnails('static/images/crochet', crochetImages.map(image => image.path))
